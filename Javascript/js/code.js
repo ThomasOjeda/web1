e7dad5f3a4f098ad58e2d05da30a7e61214f7af7
ThomasOjeda <thomasojeda05@gmail.com>
2021-04-26 11:33:26 -0300

    prende y apaga

--- Javascript/js/code.js
+++ Javascript/js/code.js
@@ -1,2 +1,21 @@
-let nombre= prompt("Cual es tu nombre?");
-document.querySelector("#id1").innerHTML = "Hola " + nombre;
+let img=document.querySelector("#foco");
+img.src="img/foco_prendido.png";
+let body=document.querySelector("#body");
+let btn_prender=document.querySelector("#btn_prender");
+let btn_apagar=document.querySelector("#btn_apagar");
+btn_prender.addEventListener("click",prender);
+btn_apagar.addEventListener("click",apagar);
+prender();
+
+function prender() {
+    img.src="img/foco_prendido.png";
+    body.classList.add("encendido");
+    body.classList.remove("apagado");
+}
+
+function apagar() {
+    img.src="img/foco_apagado.png";
+    body.classList.add("apagado");
+    body.classList.remove("encendido");
+
+}
